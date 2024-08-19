278b146291d747c6cd69dddd7101d57d3d556c42
nadamemdouh2003 <168484920+nadamemdouh2003@users.noreply.github.com>
2024-08-19 22:50:37 +0200

    Update PersonService.java

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -34,6 +34,14 @@ public class PersonService {
 		persons.add(p);
 		
 		p = new Person();
+		p.setId("10");
+		p.setAge(5);
+		p.setFirstName("ahmed");
+		p.setLastName("sn");
+		persons.add(p);
+		
+		
+		p = new Person();
 		p.setId("4");
 		p.setAge(25);
 		p.setFirstName("AbdulHalim");
